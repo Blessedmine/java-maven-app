258818578f32d507258b2fc58f3abcf5eca67c7b
unknown <blessedmine>
2025-11-11 01:46:55 -0600

    testing deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,13 @@
 terraform {
-  required_version = ">= 0.12"
-  backend "s3" {
-    bucket = "myapp-bucket"
-    key = "myapp/state.tfstate"
-    region = "us-east-1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
   }
 }
+
+# Configure the AWS Provider
 
 provider "aws" {
   region = var.region
--- terraform/variable.tf
+++ terraform/variable.tf
@@ -5,7 +5,7 @@ variable subnet_cidr_block {
   default = "10.0.10.0/24"
 }
 variable avail_zone {
-  default = "us-east-1a"
+  default = "eu-west-3a"
 }
 variable env_prefix {
   default = "dev"
@@ -20,5 +20,5 @@ variable instance_type {
   default = "t2.micro"
 }
 variable region {
-  default = "us-east-1"
+  default = "eu-west-3"
 }
