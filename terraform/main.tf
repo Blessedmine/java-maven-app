0b3541f287239d1bd1b68d24e778dd13a17c62cf
Eben Babs <Blessedmine.ayoolucool@gmail.com>
2025-11-14 09:41:19 +0100

    adding port

--- terraform/main.tf
+++ terraform/main.tf
@@ -65,6 +65,13 @@ resource "aws_default_security_group" "default-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port = 30080
+    to_port = 30080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port = 0
     to_port = 0
